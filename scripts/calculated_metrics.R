48e8601efcfd104a7c4cac04100e56dddc04cdf7
sewardlee337 <sewardlee337@gmail.com>
2016-12-03 01:29:24 -0800

    Add location quotient calculations

--- scripts/calculated_metrics.R
+++ scripts/calculated_metrics.R
@@ -21,9 +21,8 @@ CAGR <- function(begin_data, end_data, metric, years){
           (combined_table$end_metric/combined_table$begin_metric) ^(1/years) - 1
      )
      
-     cagr_table <- combined_table[, c('cluster', 'region', 'cagr')]
-     
-     return(cagr_table)
+     cagr_table <- combined_table[, c('cluster', 'region', 'cagr')] %>%
+          return()
 }
 
 ##   Region Share Calculation
@@ -34,7 +33,7 @@ RegionShare <- function(data, metric) {
      initial_table <- data[,c("cluster", "region", metric)]
      
      ##   Create dataframe with summed metric values by region
-     sum_param <- paste0("sum(", metric,")")           ##   NOTE: This is necessary to deal with NSE in summarise_() function 
+     sum_param <- paste0("sum(", metric,")")           ##   NOTE: This is necessary to deal with NSE in summarise_() function. 
      
      total_table <- group_by_(data, "region") %>%
           summarise_(sum_metric = sum_param) %>%
@@ -46,9 +45,42 @@ RegionShare <- function(data, metric) {
      ##   Calculate regional shares
      combined_table$region_share <- combined_table[,metric] / combined_table$sum_metric 
      
-     share_table <- combined_table[, c("cluster", "region", "region_share")]
-     
-     return(share_table)
+     share_table <- combined_table[, c("cluster", "region", "region_share")] %>%
+          return()
 }
 
 ##   Location Quotient Calculation
+
+LQ <- function(data, metric){
+     
+     ##   Keep relevant columns in dataset
+     initial_table <- data[,c("cluster", "region", metric)]
+     
+     ##   Create dataframe with summed metric values by region
+     sum_param <- paste0("sum(", metric,")")           ##   NOTE: This is necessary to deal with NSE in summarise_() function. 
+                                                       ##   This stupid parameter will be used in construction of both
+     region_table <- group_by_(data, "region") %>%     ##   tables to be inner-joined with intial table.
+          summarise_(sum_by_region = sum_param) %>%
+          as.data.frame()
+     
+     combined_table <- inner_join(initial_table, region_table, by = "region")
+     
+     ##   Create dataframe with summed metric values by cluster
+     cluster_table <- group_by_(data, "cluster") %>%
+          summarise_(sum_by_cluster = sum_param) %>%
+          as.data.frame()
+     
+     combined_table <- inner_join(combined_table, cluster_table, by = "cluster")
+     
+     ##   Find grand total for given metric
+     initial_table <- initial_table[which(initial_table$region != "Total"),]      ##   Remove rows with "Total" to prevent double-counting.
+
+     grand_total <- sum(initial_table[,metric])
+     
+     ##   Calculate location quotient
+     combined_table$location_quotient <- (combined_table[,metric] / combined_table$sum_by_region) / (combined_table$sum_by_cluster / grand_total)
+     
+     lq_table <- combined_table[, c("cluster", "region", "location_quotient")] %>%
+          return()
+     
+}
